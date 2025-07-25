6d4567acd033b309ccd05b29e0a3fc154d32511b
Axel Giovanni Ojeda Hernandez <aojedah1800@alumno.ipn.mx>
2025-07-25 17:27:41 -0600

    primera version

--- app.js
+++ app.js
@@ -1,1 +1,61 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+let amigo;
+
+const listaAmigosSecretos = document.getElementById("listaAmigos");
+const listaAmigoSorteado = document.getElementById("resultado");
+
+function limpiarInput() {
+    document.getElementById("amigo").value = "";
+}
+
+function obtenerAmigo() {
+    amigo = document.getElementById("amigo").value;
+
+    if(amigo === "") {
+        alert("Por favor, ingresa un nombre de amigo valido.");
+        limpiarInput();
+        return;
+    }
+    if(amigo.length < 3 || amigo.length > 20) {
+        alert("El nombre debe tener entre 3 y 20 caracteres.");
+        limpiarInput();
+        return;
+    }
+    limpiarInput();
+    return amigo;
+    
+}
+
+function agregarAmigo() {
+    const listaAmigosElemento = document.createElement("li");
+
+    if(obtenerAmigo() === undefined) {
+        return;
+    } 
+    
+    listaAmigos.push(amigo);
+
+    listaAmigosElemento.textContent = amigo;
+    listaAmigosSecretos.appendChild(listaAmigosElemento);
+
+    document.querySelector(".button-draw").disabled = false;
+}
+
+function sortearAmigo() {
+    const apuntador = Math.floor(Math.random() * listaAmigos.length);
+    const amigoSorteado = listaAmigos[apuntador];
+    const amigoSorteadoElemento = document.createElement("li");
+
+    listaAmigos = []
+
+    if(listaAmigoSorteado.children.length > 0) {
+        listaAmigoSorteado.innerHTML = "";
+    }
+
+    listaAmigosSecretos.innerHTML = "";
+
+    amigoSorteadoElemento.textContent = amigoSorteado;
+    listaAmigoSorteado.appendChild(amigoSorteadoElemento);
+
+    document.querySelector(".button-draw").disabled = true;
+}
